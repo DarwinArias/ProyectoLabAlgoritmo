22a1974aa8fff40b945c5acb581abeb8ccc75778
JoseB21 <josebolivar.2117@gmail.com>
2019-07-14 10:47:51 -0400

    menujuego

--- Snake/main.c
+++ Snake/main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int menuMundos()
+{
+
+        char menu1 = '0';
+        printf(" \t|********************************************|\n");
+        printf(" \t|                                            |\n");
+        printf(" \t|                  NIVELES                   |\n");
+        printf(" \t|                                            |\n");
+        printf(" \t|  ****  ****  ****  ****  ****  ****  ****  |\n");
+        printf(" \t|***  ****  ****  ****  ****  ****  ****  ***|\n");
+        printf(" \t|                                            |\n");
+        printf(" \t| 1- Primer nivel                            |\n");
+        printf(" \t| 2- Segundo nivel                           |\n");
+        printf(" \t| 3- Menu inicial                            |\n");
+        printf(" \t|____________________________________________|\n");
+        scanf("%c", &menu1);
+         menu1=(getch());
+        switch (menu1)
+        {
+        case '1':
+            system("cls");
+            jugar();
+
+            break;
+
+        case '2':
+                system ("cls");
+                jugar2();
+            break;
+
+        case '3':
+            system("cls");
+            Menuinicial();
+            break;
+         default:
+            system ("cls");
+            printf("Opcion no valida. Inserte una opcion valida.\n\n");
+            menuMundos();
+            break;
+    }
+
+}
 int main()
 {
     printf("Hello world!\n");

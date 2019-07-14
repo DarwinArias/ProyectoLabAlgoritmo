a097d4ee0503c5a1840c500377fe6acdebacbd64
DarwinArias <50183897+DarwinArias@users.noreply.github.com>
2019-07-14 11:20:26 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Snake/bin/Debug/Snake.exe
    #	Snake/main.c
    #	Snake/obj/Debug/main.o

--- Snake/main.c
+++ Snake/main.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-
-int menuMundos()
+#include <conio.h>
+#include <windows.h>
+int main()
+{
+    menuMundos();
+    printf("\n");
+    return 0;
+}
+void menuMundos()
 {
 
         char menu1 = '0';
-        printf(" \t|********************************************|\n");
-        printf(" \t|                                            |\n");
-        printf(" \t|                  NIVELES                   |\n");
-        printf(" \t|                                            |\n");
-        printf(" \t|  ****  ****  ****  ****  ****  ****  ****  |\n");
-        printf(" \t|***  ****  ****  ****  ****  ****  ****  ***|\n");
-        printf(" \t|                                            |\n");
-        printf(" \t| 1- Primer nivel                            |\n");
-        printf(" \t| 2- Segundo nivel                           |\n");
-        printf(" \t| 3- Menu inicial                            |\n");
-        printf(" \t|____________________________________________|\n");
-        scanf("%c", &menu1);
+        printf(" \t\t|********************************************|\n");
+        printf(" \t\t|                                            |\n");
+        printf(" \t\t|                  NIVELES                   |\n");
+        printf(" \t\t|                                            |\n");
+        printf(" \t\t|  ****  ****  ****  ****  ****  ****  ****  |\n");
+        printf(" \t\t|***  ****  ****  ****  ****  ****  ****  ***|\n");
+        printf(" \t\t|                                            |\n");
+        printf(" \t\t| 1- Primer nivel                            |\n");
+        printf(" \t\t| 2- Segundo nivel                           |\n");
+        printf(" \t\t| 3- Menu inicial                            |\n");
+        printf(" \t\t|____________________________________________|\n");
          menu1=(getch());
         switch (menu1)
         {
         case '1':
             system("cls");
-            jugar();
-
+//            jugar();
+            world_style();
             break;
 
         case '2':
                 system ("cls");
-                jugar2();
+//                jugar2();
             break;
 
         case '3':
             system("cls");
-            Menuinicial();
+//            Menuinicial();
             break;
          default:
             system ("cls");
@@ -42,19 +47,8 @@ int menuMundos()
             menuMundos();
             break;
     }
-
+    system("color f5");
 }
-=======
-#include <conio.h>
-#include <windows.h>
->>>>>>> origin/master
-int main()
-{
-    world_style();
-    printf("\n");
-    return 0;
-}
-
 void gotoxy (int x, int y)
 {
     HANDLE hCon;
@@ -71,52 +65,29 @@ void world_style()
 {
     int i;
     //Parte Superior Capa 1
-    for(i=0; i<=110; i++)
+    for(i=20; i<=110; i++)
     {
-        gotoxy (i,0);
+        gotoxy (i,3);
         printf("%c", 177);
     }
     //Parte Izquierda Capa 1
-    for(i=0; i<=20; i++)
+    for(i=3; i<=20; i++)
     {
-        gotoxy (0,i);
+        gotoxy (19,i);
          printf("%c", 177);
     }
     //Parte Derecha Capa 1
-    for(i=0; i<=20; i++)
+    for(i=3; i<=20; i++)
     {
         gotoxy (110,i);
         printf("%c", 177);
     }
     //Parte Abajo Capa 1
-    for(i=0; i<=110; i++)
+    for(i=20; i<=110; i++)
     {
         gotoxy (i,20);
         printf("%c", 177);
     }
-    //Parte Superior Capa 2
-    for(i=0; i<=110; i++)
-    {
-        gotoxy (i,1);
-         printf("%c", 177);
-    }
-    //Parte Izquierda Capa 2
-    for(i=0; i<=20; i++)
-    {
-        gotoxy (1,i);
-        printf("%c", 177);
-    }
-    //Parte Derecha Capa 2
-    for(i=0; i<=20; i++)
-    {
-        gotoxy (109,i);
-        printf("%c", 177);
-    }
-    //Parte Abajo Capa 2
-    for(i=0; i<=110; i++)
-    {
-        gotoxy (i,19);
-        printf("%c", 177);
-    }
     system("color f5");
 }
+

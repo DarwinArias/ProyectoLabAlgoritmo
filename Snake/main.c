5e4b002a64499ad8f381b57a9731ddedfe89d886
JoseB21 <josebolivar.2117@gmail.com>
2019-07-14 11:28:49 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Snake/bin/Debug/Snake.exe
    #	Snake/main.c
    #	Snake/obj/Debug/main.o

--- Snake/main.c
+++ Snake/main.c
@@ -1,53 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-
-int menuMundos()
-{
-
-        char menu1 = '0';
-        printf(" \t|********************************************|\n");
-        printf(" \t|                                            |\n");
-        printf(" \t|                  NIVELES                   |\n");
-        printf(" \t|                                            |\n");
-        printf(" \t|  ****  ****  ****  ****  ****  ****  ****  |\n");
-        printf(" \t|***  ****  ****  ****  ****  ****  ****  ***|\n");
-        printf(" \t|                                            |\n");
-        printf(" \t| 1- Primer nivel                            |\n");
-        printf(" \t| 2- Segundo nivel                           |\n");
-        printf(" \t| 3- Menu inicial                            |\n");
-        printf(" \t|____________________________________________|\n");
-        scanf("%c", &menu1);
-         menu1=(getch());
-        switch (menu1)
-        {
-        case '1':
-            system("cls");
-            jugar();
-
-            break;
-
-        case '2':
-                system ("cls");
-                jugar2();
-            break;
-
-        case '3':
-            system("cls");
-            Menuinicial();
-            break;
-         default:
-            system ("cls");
-            printf("Opcion no valida. Inserte una opcion valida.\n\n");
-            menuMundos();
-            break;
-    }
-
-}
-=======
 #include <conio.h>
 #include <windows.h>
->>>>>>> origin/master
+
+int tama=3;
+int snake [50][2];
+int t=1;
+int x=10;
+int y=12;
+
 int main()
 {
     world_style();
@@ -80,7 +41,7 @@ void world_style()
     for(i=0; i<=20; i++)
     {
         gotoxy (0,i);
-         printf("%c", 177);
+        printf("%c", 177);
     }
     //Parte Derecha Capa 1
     for(i=0; i<=20; i++)
@@ -98,7 +59,7 @@ void world_style()
     for(i=0; i<=110; i++)
     {
         gotoxy (i,1);
-         printf("%c", 177);
+        printf("%c", 177);
     }
     //Parte Izquierda Capa 2
     for(i=0; i<=20; i++)
@@ -119,4 +80,63 @@ void world_style()
         printf("%c", 177);
     }
     system("color f5");
+}
+
+void DibujarSnake()
+{
+    int i;
+    for (i=1; i<tama; i++)
+    {
+        gotoxy(snake[i][0],snake[i][1]);
+        printf("o");
+    }
+}
+
+void BorrarSnake()
+{
+    gotoxy(snake[t][0],snake[t][1]);
+    printf(" ");
+}
+
+void menuMundos()
+{
+
+    char menu1 = '0';
+    printf(" \t|********************************************|\n");
+    printf(" \t|                                            |\n");
+    printf(" \t|                  NIVELES                   |\n");
+    printf(" \t|                                            |\n");
+    printf(" \t|  ****  ****  ****  ****  ****  ****  ****  |\n");
+    printf(" \t|***  ****  ****  ****  ****  ****  ****  ***|\n");
+    printf(" \t|                                            |\n");
+    printf(" \t| 1- Primer nivel                            |\n");
+    printf(" \t| 2- Segundo nivel                           |\n");
+    printf(" \t| 3- Menu inicial                            |\n");
+    printf(" \t|____________________________________________|\n");
+    scanf("%c", &menu1);
+    menu1=(getch());
+    switch (menu1)
+    {
+    case '1':
+        system("cls");
+        jugar();
+
+        break;
+
+    case '2':
+        system ("cls");
+        jugar2();
+        break;
+
+    case '3':
+        system("cls");
+        Menuinicial();
+        break;
+    default:
+        system ("cls");
+        printf("Opcion no valida. Inserte una opcion valida.\n\n");
+        menuMundos();
+        break;
+    }
+
 }
